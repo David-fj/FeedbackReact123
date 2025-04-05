6033778bd9d47a9ae76c0bf721e89e40c2d596c6
andl2008 <andl2008@hotmail.com>
2025-04-05 14:23:45 +0000

    Ajuste da estilização Desktop e fim do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -32,24 +32,24 @@ export function App() {
       feedback is apreciated to help us improve our offering!</p>
 
       <div className="flex justify-between mb-6">
-        <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray focus:bg-medium-gray focus:text-white font-bold" onClick={() => handleChangeRateNote(1)}>1</button>
-        <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray focus:bg-medium-gray focus:text-white font-bold" onClick={() => handleChangeRateNote(2)}>2</button>
-        <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray focus:bg-medium-gray focus:text-white font-bold" onClick={() => handleChangeRateNote(3)}>3</button>
-        <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray focus:bg-medium-gray focus:text-white font-bold" onClick={() => handleChangeRateNote(4)}>4</button>
-        <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray focus:bg-medium-gray focus:text-white font-bold" onClick={() => handleChangeRateNote(5)}>5</button>
+        <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray focus:bg-medium-gray focus:text-white font-bold hover:bg-orange hover:text-white cursor-pointer duration-400 sm:w-13 sm:h-13" onClick={() => handleChangeRateNote(1)}>1</button>
+        <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray focus:bg-medium-gray focus:text-white font-bold hover:bg-orange hover:text-white cursor-pointer duration-400 sm:w-13 sm:h-13" onClick={() => handleChangeRateNote(2)}>2</button>
+        <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray focus:bg-medium-gray focus:text-white font-bold hover:bg-orange hover:text-white cursor-pointer duration-400 sm:w-13 sm:h-13" onClick={() => handleChangeRateNote(3)}>3</button>
+        <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray focus:bg-medium-gray focus:text-white font-bold hover:bg-orange hover:text-white cursor-pointer duration-400 sm:w-13 sm:h-13" onClick={() => handleChangeRateNote(4)}>4</button>
+        <button className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm text-medium-gray focus:bg-medium-gray focus:text-white font-bold hover:bg-orange hover:text-white cursor-pointer duration-400 sm:w-13 sm:h-13" onClick={() => handleChangeRateNote(5)}>5</button>
       </div>
 
-      <button onClick={handleSubmit} className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1">Submit</button>
+      <button onClick={handleSubmit} className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1 hover:bg-white hover:text-orange duration-400 cursor-pointer">Submit</button>
     </div>
     ) : (
       <div className="bg-radial-gradient text-white rounded-2xl p-6 mx-w-103 font-overpass">
         <img className ="mx-auto mb-6" src={illustrationThankYou} alt="Ilustration Thank You" />
         
-        <p className="bg-dark-blue text-orange w-fit mx-auto py-1.25 px-3 rounded-3xl mb-6">You selected 4 out of 5 </p>
+        <p className="bg-dark-blue text-orange w-fit mx-auto py-1.25 px-3 rounded-3xl mb-6">You selected {rateNote} out of 5 </p>
 
-        <h1 className="text-2xl font-bold mb-2.5">Thank you</h1>
+        <h1 className="text-2xl font-bold mb-2.5 text-center">Thank you</h1>
 
-        <p className="text-sm text-light-gray leading-1">We 
+        <p className="text-sm text-light-gray leading-1 text-center">We 
         appreciate you taking the time to give a rating. If you ever 
         need more support, don’t hesitate to get in touch!</p>
       </div>
