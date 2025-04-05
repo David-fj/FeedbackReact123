7498110d04718024c840928586479706cbf0b2f3
andl2008 <andl2008@hotmail.com>
2025-04-05 14:04:44 +0000

    Fim da estilização do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import iconStar from "./assets/icon-star.svg"
+import illustrationThankYou from "./assets/illustration-thank-you.svg"
 import { useState } from "react"
 
 export function App() {
@@ -42,7 +43,15 @@ export function App() {
     </div>
     ) : (
       <div className="bg-radial-gradient text-white rounded-2xl p-6 mx-w-103 font-overpass">
-        <p>Enviou tudo </p>
+        <img className ="mx-auto mb-6" src={illustrationThankYou} alt="Ilustration Thank You" />
+        
+        <p className="bg-dark-blue text-orange w-fit mx-auto py-1.25 px-3 rounded-3xl mb-6">You selected 4 out of 5 </p>
+
+        <h1 className="text-2xl font-bold mb-2.5">Thank you</h1>
+
+        <p className="text-sm text-light-gray leading-1">We 
+        appreciate you taking the time to give a rating. If you ever 
+        need more support, don’t hesitate to get in touch!</p>
       </div>
     )
 
